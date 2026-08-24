918fe1b54c8b34300e06185924daf2da5435bb3d
Furnica Albert <albertfurnica2005@gmail.com>
2026-08-24 21:54:21 +0300

    Enhance homepage layout and functionality: add random movie loading, improve styles, and implement new hero section with features

--- 01_MovieTracker/wwwroot/Scripts/home.js
+++ 01_MovieTracker/wwwroot/Scripts/home.js
@@ -1,1 +1,39 @@
 localStorage.clear();
+
+const API_URL_MOVIES = "http://localhost:5160/api/movies";
+
+async function loadRandomMovies(){
+    try{
+        const response = await fetch(API_URL_MOVIES);
+        const allMovies = await response.json();
+        const shuffledMovies = allMovies.sort(() => 0.5 - Math.random());
+        const selectedMovies = shuffledMovies.slice(0, 5);
+
+        renderPreviewMovies(selectedMovies);
+    } catch(error){
+        console.error("Error while randomizing movies : ", error);
+    }
+}
+
+function renderPreviewMovies(movies){
+    const container = document.getElementById("randomMoviesGrid");
+    container.html = "";
+
+    movies.forEach(m => {
+        const card = document.createElement("div");
+        card.className = "preview-card";
+
+        const imageSrc = m.posterUrl ? m.posterUrl : "Photos/home_logo.jfif";
+
+        card.innerHTML = `
+            <div class="preview-poster">
+                <img src="${imageSrc}" alt="${m.title}">
+            </div>
+            <h3 classs="preview-title">${m.title}</h3>
+        `;
+
+        container.appendChild(card);
+    })
+}
+
+loadRandomMovies();
