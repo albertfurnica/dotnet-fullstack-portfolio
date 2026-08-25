b590c198ef8fb2e403262a55c3b604aee5264867
Furnica Albert <albertfurnica2005@gmail.com>
2026-08-25 18:52:06 +0300

    Add a form for sign in/register inside home page

--- 01_MovieTracker/wwwroot/Scripts/home.js
+++ 01_MovieTracker/wwwroot/Scripts/home.js
@@ -1,6 +1,116 @@
 localStorage.clear();
 
 const API_URL_MOVIES = "http://localhost:5160/api/movies";
+const btnSignIn = document.getElementById("btnSignIn");
+const btnRegister = document.getElementById("btnRegister");
+const modal = document.getElementById("addModal");
+const modalTitle = document.getElementById("modalTitle");
+const modalClose = document.getElementById("closeModal");
+const signInForm = document.getElementById("signInForm");
+const modalBody = document.getElementById("modalBody");
+const closeModal = document.getElementById("closeModal");
+
+closeModal.addEventListener("click", function(){
+    modal.style.display = "none";
+})
+
+btnSignIn.addEventListener("click", function(){
+    modal.style.display = "flex";
+    modalTitle.innerHTML = "Introduce your credentials";
+    modalBody.innerHTML = "";
+    modalBody.innerHTML = `
+        <div class="form-group">
+            <label>Introduce your email:</label>
+            <input type="email" id="loginEmail" required>
+        </div>
+        <div class="form-group">
+            <label>Introduce your password:</label>
+            <input type="password" id="loginPassword" required>
+            <div  class="reset-password-container">
+                <a href="resetPassword.html" target="_blank">Forgot your password?</a>
+            </div>
+        </div>
+        <footer class="modal-footer">
+            <button type="submit" class="btn-login">Login</button>
+            <div class="register-section">
+                <p>No account? <a onclick="btnRegister.click()">Register!</a></p>
+            </div>    
+        </footer>
+    `;
+})
+
+btnRegister.addEventListener("click", function(){
+    modal.style.display = "flex";
+    modalTitle.innerHTML = "Join MovieTracker";
+    modalBody.innerHTML = "";
+    modalBody.innerHTML = `
+        <div class="form-group">
+            <label>Introduce your last name:</label>
+            <input type="text" id="userLastName" required pattern="[A-Za-z]+" title="Only letters are allowed!">
+        </div>
+
+        <div class="form-group">
+            <label>Introduce your First name:</label>
+            <input type="text" id="userFirstName" required pattern="[A-Za-z]+" title="Only letters are allowed!">
+        </div>
+
+        <div class="form-group">
+            <label>Introduce your email:</label>
+            <input type="email" id="userEmail" required>
+        </div>
+
+        <div class="form-group">
+            <label>Introduce your password:</label>
+            <input type="password" id="userPassword" required minlength="4" title="Password must be at least 8 characters long!">
+        </div>
+
+        <footer class="modal-footer">
+            <button type="submit" class="btn-login">Register</button>
+            <div class="register-section">
+                <p>Already have an account? <a onclick="btnSignIn.click()">Sign in!</a></p>
+            </div>
+        </footer>
+    `;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 async function loadRandomMovies(){
     try{
